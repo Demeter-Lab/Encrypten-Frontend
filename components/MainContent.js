590fa17a8732f394243062f126c56c9daddfb0ae
Mide001 <olamideisreal5@gmail.com>
2024-02-12 16:43:23 +0100

    landing page

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,19 +1,37 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      className="flex items-center justify-center"
+      style={{ minHeight: "80vh" }}
+    >
+      <MainContent />
+    </div>
+  );
+};
+
 const MainContent = () => {
   return (
     <>
       <div className="p-4 rounded-lg text-center">
-        <h2 className="text-[24px] sm:text-[44px] text-[#170F49] font-semibold leading-[1] mt-4">
-          DAO-Based Ecosystem For
+        <h2 className="text-[24px] sm:text-[44px] text-gray-200 font-semibold leading-[1] mt-4">
+          Encrypten Is A DAO-Based Ecosystem For
           <br className="hidden sm:inline" /> Tenprotocol!
         </h2>
-        <p className="text-[12px] sm:text-[12px] text-[#6F6C90] mt-4">
-          Simplify workforce payments with our versatile crypto payroll system,
-          offering flexibility and <br className="hidden sm:inline" />{" "}
-          convenience for both single and batch transactions.
+        <p className="text-[12px] sm:text-[12px] text-gray-400 mt-4">
+          Redefining Decentralized Governance for Web3
+          <br className="hidden sm:inline" /> Unlock the power of decentralized
+          decision-making with Encrypten – your gateway to transparent, secure,
+          and community-driven governance in the Web3 era
         </p>
+
+        <button className="text-[#203475] hover:text-gray-300 bg-gray-300 hover:bg-black px-4 py-1 mt-6">
+          Launch App
+        </button>
       </div>
     </>
   );
 };
 
-export default MainContent;
+export default Home;
